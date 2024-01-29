abfbae328432536d6c2a6aee1a1affd4e2f6a0d4
Reza Farahani <reza.khmf@gmail.com>
2024-01-30 10:45:34 +1100

    Initial Commit

---
+++ Coles/ColesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColesApp.swift
+//  Coles
+//
+//  Created by Reza Farahani on 30/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct ColesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
